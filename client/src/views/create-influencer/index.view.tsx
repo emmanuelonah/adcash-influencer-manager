065673ec7e1495c94fa36f384a116186057a48db
Emmanuel Onah <37575223+emmanuelonah@users.noreply.github.com>
2025-04-13 18:09:20 +0200

    feat: add back button from create-influencer page

--- client/src/views/create-influencer/index.view.tsx
+++ client/src/views/create-influencer/index.view.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from 'routes';
 import { PageWrapper } from 'layouts';
 
 import { Form } from './components/form';
@@ -6,6 +9,7 @@ export function CreateInfluencer() {
   return (
     <PageWrapper>
       <title>📸 Create influencer | Adcash influencer manager</title>
+      <Link to={ROUTES.home}>👈 Back</Link>
       <Form />
     </PageWrapper>
   );
