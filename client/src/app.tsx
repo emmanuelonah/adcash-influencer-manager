0535c4dfec773f9e76fd7c200d31681c845d96ba
Emmanuel Onah <37575223+emmanuelonah@users.noreply.github.com>
2025-04-13 16:45:54 +0200

    fix: omg did really push the error observability tester hahaha

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,14 +1,7 @@
 import { Routes } from 'routes';
 import { useCreateErrorLogPresenter } from 'models';
-import { useEffect } from 'react';
 
 export function App() {
-  useEffect(() => {
-    setTimeout(() => {
-      throw new Error('This is a test error');
-    }, 5000);
-  }, []);
-
   useCreateErrorLogPresenter();
 
   return <Routes />;
